3d05ab8265eed993ac6ff2b4d307228a59bf9f52
Lionel Rowe <lionel.rowe@gmail.com>
2026-09-09 16:52:12 +0100

    Better watch behaviour for build task

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,34 +1,53 @@
 import { replaceAllAsync } from '@std/regexp/unstable-replace-all-async'
 import { typeByExtension } from '@std/media-types'
+import { debounce } from '@std/async/debounce'
+
+const isWatchMode = Deno.args.includes('--watch') || Deno.args.includes('-w')
 
 function resolve(path: string) {
 	return new URL(import.meta.resolve(path)).pathname
 }
 
-const proc = new Deno.Command('deno', {
-	args: ['bundle', './src/main.ts'],
-	stdout: 'piped',
-}).spawn()
+const build = debounce(async () => {
+	const proc = new Deno.Command('deno', {
+		args: ['bundle', './src/main.ts'],
+		stdout: 'piped',
+	}).spawn()
 
-let output = new TextDecoder().decode((await proc.output()).stdout)
+	let output = new TextDecoder().decode((await proc.output()).stdout)
 
-output = await replaceAllAsync(
-	output,
-	/\{\{\s*@datauri\s+(.+?)\s*\}\}/g,
-	async (_, path) =>
-		`data:${typeByExtension(path.split('.').pop() ?? '')};base64,${
-			new Uint8Array(await Deno.readFile(resolve(path))).toBase64()
-		}`,
-)
-
-output = await replaceAllAsync(output, /(['"])\{\{\s*@html\s+(.+?)\s*\}\}\1/g, async (_, _2, path) => {
-	return JSON.stringify(
-		await replaceAllAsync(
-			await Deno.readTextFile(resolve(path)),
-			/<!--\s*@stylesheet\s+(.+?)\s*-->/g,
-			async (_, path) => `<style>${await Deno.readTextFile(resolve(path))}</style>`,
-		),
+	output = await replaceAllAsync(
+		output,
+		/\{\{\s*@datauri\s+(.+?)\s*\}\}/g,
+		async (_, path) =>
+			`data:${typeByExtension(path.split('.').pop() ?? '')};base64,${
+				new Uint8Array(await Deno.readFile(resolve(path))).toBase64()
+			}`,
 	)
-})
 
-await Deno.writeTextFile('./dist/main.js', output)
+	output = await replaceAllAsync(output, /(['"])\{\{\s*@html\s+(.+?)\s*\}\}\1/g, async (_, _2, path) => {
+		return JSON.stringify(
+			await replaceAllAsync(
+				await Deno.readTextFile(resolve(path)),
+				/<!--\s*@stylesheet\s+(.+?)\s*-->/g,
+				async (_, path) => `<style>${await Deno.readTextFile(resolve(path))}</style>`,
+			),
+		)
+	})
+
+	await Deno.writeTextFile('./dist/main.js', output)
+}, 200)
+
+build()
+
+if (isWatchMode) {
+	await watch()
+}
+
+async function watch() {
+	for await (const event of Deno.watchFs(['./src', './static'])) {
+		if (event.kind === 'modify') {
+			build()
+		}
+	}
+}
